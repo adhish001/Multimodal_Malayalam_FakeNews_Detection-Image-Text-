e08009e8ad8ef1acb46721c023b2de6463474c6d
adhish001 <127467593+adhish001@users.noreply.github.com>
2024-03-24 23:32:53 +0530

    Updating streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -126,7 +126,16 @@ fusion_model = load_model(selected_model)
 
 if page == "Main Page":
     st.header("Main Page Content")
-    st.write("This is the main page. Use the sidebar to navigate.")
+    st.write("Welcome to our Multimodal Fake News Detection WebApp!")
+    st.write("Detecting fake news in the Malayalam language has become increasingly challenging in today's digital age. With the rapid spread of misinformation and manipulated content, distinguishing between truth and falsehood has never been more crucial.")
+    st.write("Our innovative web application tackles this problem head-on by utilizing the power of both text and image modalities. We understand that fake news often involves not just misleading text but also manipulated visuals, making it imperative to analyze both aspects simultaneously.")
+    st.write("Through our user-friendly interface, simply input the text and the corresponding image associated with the news article you want to verify. Our advanced algorithms then meticulously analyze both the textual and visual components to determine the authenticity of the news.")
+    st.write("Say goodbye to misinformation. Empower yourself with truth!")
+
+    # Option to show an image
+    if st.checkbox("Show Image"):
+        st.image("your_image.jpg", caption="Your Image Caption")
+
 
 elif page == "Predict Fake News":
     st.header("Predict Fake News")
